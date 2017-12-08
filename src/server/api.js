bfa4532e5ab1041243169ed48bdc4617036aa2d6
ston3o <contact@ston3o.me>
2017-12-09 00:47:30 +0100

    Add wordpress and ghost charts

--- src/server/api.js
+++ src/server/api.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import yaml from 'yamljs';
 
 const api = express();
 
 api.get('/', (req, res) => {
     res.json({ response: 'Hello api!' });
+});
+
+api.get('/charts', (req, res) => {
+    const chartIndex = yaml.load(`${__dirname}/../../charts/index.yaml`);
+    const charts = Object.values(chartIndex.entries).map(chart => ({ name: chart[0].name, icon: chart[0].icon }));
+
+    res.json(charts);
 });
 
 export default api;
