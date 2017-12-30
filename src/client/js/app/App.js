4d83df327e7276afe6cee79784f3d7b7b0e39861
ston3o <contact@ston3o.me>
2017-12-30 18:54:40 +0100

    Change slogan

--- src/client/js/app/App.js
+++ src/client/js/app/App.js
@@ -12,9 +12,12 @@ const App = () => {
 
             <Header as="h1">
                 <img src={logo} alt="logo" />
-                <Header.Content>Ethibox</Header.Content>
+                <Header.Content>
+                    Ethibox
+                    <Header.Subheader>Let&apos;s decentralize the internet!</Header.Subheader>
+                </Header.Content>
                 <a href="https://github.com/ston3o/ethibox" target="_blank" rel="noopener noreferrer">
-                    <img style={{ position: 'absolute', top: 0, right: 0, border: 0 }} src="http://bit.ly/2Cci7hn" alt="Fork me on GitHub" data-canonical-src="http://bit.ly/2Cdw7aQ" />
+                    <img style={{ position: 'absolute', top: 0, right: 0, border: 0 }} src="https://frama.link/fork-me" alt="Fork me on GitHub" data-canonical-src="https://frama.link/wKZ3Ly2G" />
                 </a>
             </Header>
 
