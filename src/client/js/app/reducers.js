92f7b4ebddd2ef9d6cb28d1fb3e2b21715ae34e2
ston3o <contact@ston3o.me>
2018-01-03 19:52:49 +0100

    Remove unused reducer

--- src/client/js/app/reducers.js
+++ src/client/js/app/reducers.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux';
 import ApplicationReducer from '../application/ApplicationReducer';
-import ChartReducer from '../chart/ChartReducer';
 import ModalReducer from '../modal/ModalReducer';
 import LoaderReducer from '../loader/LoaderReducer';
 
-const reducers = combineReducers({ ApplicationReducer, ChartReducer, ModalReducer, LoaderReducer });
+const reducers = combineReducers({ ApplicationReducer, ModalReducer, LoaderReducer });
 
 export default reducers;
