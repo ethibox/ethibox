4ea089589cbd87caaeaee91b6fc0792a8c6a053b
johackim <contact@johackim.com>
2018-01-03 19:52:49 +0100

    Remove unused reducer

--- src/client/js/chart/ChartActions.js
+++
@@ -1,7 +0,0 @@
-export const listChartsSuccess = charts => ({ type: 'LIST_CHARTS_SUCCESS', charts });
-
-export const listCharts = () => (dispatch) => {
-    fetch('/api/charts')
-        .then(data => data.json())
-        .then(charts => dispatch(listChartsSuccess(charts)));
-};
