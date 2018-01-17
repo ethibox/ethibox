2def4a37f67c5ea8a9d44077aa94794fe9846f3f
ston3o <contact@ston3o.me>
2018-01-17 11:23:00 +0100

    Add forgotten file

---
+++ src/client/js/app/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+import logo from '../../images/logo.svg';
+
+export default (props) => {
+    return (
+        <Header as="h1" floated={props.floated}>
+            <img src={logo} alt="logo" />
+            <Header.Content>
+                Ethibox
+                <Header.Subheader>Let&apos;s decentralize the internet!</Header.Subheader>
+            </Header.Content>
+        </Header>
+    );
+};
