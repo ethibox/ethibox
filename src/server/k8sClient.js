3bcaf042fee92df40470baa0f284d83918451aba
ston3o <contact@ston3o.me>
2018-01-19 23:36:57 +0100

    Remove checkConfig function

--- src/server/k8sClient.js
+++ src/server/k8sClient.js
@@ -11,20 +11,6 @@ const SWIFT_ENDPOINT = `http://${process.env.SWIFT_SERVICE_HOST}:${process.env.S
 const CHART_REPOSITORY = process.env.CHART_REPOSITORY || `http://${process.env.ETHIBOX_SERVICE_HOST}:${process.env.ETHIBOX_SERVICE_PORT}/charts`;
 const NAMESPACE = 'default';
 
-export const checkConfig = () => {
-    if (!process.env.TOKEN) {
-        throw new Error('No kubernetes token');
-    }
-
-    if (!process.env.SWIFT_SERVICE_HOST || !process.env.SWIFT_SERVICE_PORT_PT) {
-        throw new Error('Swift configuration error');
-    }
-
-    if (!process.env.KUBERNETES_SERVICE_HOST) {
-        throw new Error('Kubernetes configuration error');
-    }
-};
-
 export const listCharts = () => {
     const chartIndex = yaml.load(path.join(__dirname, (process.env.NODE_ENV === 'production') ? '../' : '../../', 'charts/packages/index.yaml'));
     return Object.values(chartIndex.entries).map(chart => ({ name: chart[0].name, icon: chart[0].icon, category: chart[0].keywords[0] }));
