fb7a4923c3bd02a55f03876b0cb011e853c63d5f
ston3o <contact@ston3o.me>
2018-01-25 20:07:46 +0100

    Add message demo

--- src/client/js/app/App.js
+++ src/client/js/app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Divider, Card, Button } from 'semantic-ui-react';
 import ApplicationList from '../application/ApplicationList';
 import ChartList from '../chart/ChartList';
+import MessageDemo from './MessageDemo';
 import Modal from '../modal/Modal';
 import Loader from '../loader/Loader';
 import Header from './Header';
@@ -16,6 +17,8 @@ const App = () => {
             <Header floated="left" />
             <Button basic floated="right" icon="sign out" content="Se déconnecter" onClick={() => { localStorage.clear(); window.location.replace('/'); }} />
             <Divider hidden clearing />
+
+            { process.env.DEMO && <MessageDemo /> }
 
             <Card.Group itemsPerRow={4} stackable>
                 <ApplicationList />
