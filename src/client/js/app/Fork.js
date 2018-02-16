7cda14a403e625876e86238c1d4e127f440d86a2
ston3o <contact@ston3o.me>
2018-02-16 12:30:32 +0100

    Update dashboard layout

--- src/client/js/app/Fork.js
+++ src/client/js/app/Fork.js
@@ -3,8 +3,8 @@ import forkMe from '../../images/fork-me.png';
 
 export default () => {
     return (
-        <a href="https://github.com/ston3o/ethibox" target="_blank" rel="noopener noreferrer">
-            <img style={{ position: 'absolute', top: 0, right: 0, border: 0 }} src={forkMe} alt="fork-me" />
+        <a href="https://github.com/ston3o/ethibox" target="_blank" rel="noopener noreferrer" className="fork">
+            <img style={{ position: 'absolute', top: 0, right: 0, border: 0, zIndex: 100 }} src={forkMe} alt="fork-me" />
         </a>
     );
 };
