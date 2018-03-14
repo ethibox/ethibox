492f2961903813cd94fdf41c076e2743062471c7
ston3o <contact@ston3o.me>
2018-03-14 20:53:36 +0100

    Initial commit

--- src/client/js/application/Application.js
+++ src/client/js/application/Application.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Modal, Header, Card, Image, Button, Icon, Dimmer, Loader } from 'semantic-ui-react';
+import { Input, Dropdown, Modal, Header, Card, Image, Button, Icon, Dimmer, Loader } from 'semantic-ui-react';
+import isFQDN from 'validator/lib/isFQDN';
 import { uninstallApplication } from '../application/ApplicationActions';
 
 const RUNNING = 'running';
@@ -9,11 +10,23 @@ const LOADING = 'loading';
 const ERROR = 'error';
 
 class Application extends React.Component {
-    state = { action: '' };
+    state = { action: '', domainName: '', error: false };
 
     uninstall = () => {
         this.setState({ action: '' });
         this.props.uninstallApplication(this.props.releaseName);
+    }
+
+    enterDomainName = (key) => {
+        const { domainName } = this.state;
+
+        if (key === 'Enter') {
+            if (isFQDN(domainName)) {
+                // this.editDomainName(domainName.trim());
+            } else {
+                this.setState({ error: true });
+            }
+        }
     }
 
     renderDescription = () => {
@@ -41,6 +54,36 @@ class Application extends React.Component {
         );
     }
 
+    renderButtons = () => {
+        const { action, domainName, error } = this.state;
+        const options = [
+            { key: 'edit', icon: 'edit', text: 'Edit domain name', value: 'edit', onClick: () => this.setState({ action: 'editDomainName' }) },
+        ];
+
+        if (action === 'editDomainName') {
+            return (
+                <Input
+                    error={error}
+                    onBlur={() => this.setState({ action: '' })}
+                    value={domainName}
+                    onChange={(e, data) => this.setState({ domainName: data.value, error: false })}
+                    onKeyDown={e => this.enterDomainName(e.key)}
+                    placeholder="Enter domain name..."
+                    transparent
+                    autoFocus
+                    fluid
+                />
+            );
+        }
+
+        return (
+            <Button.Group color="red" widths={2}>
+                <Button style={{ width: '90%' }} onClick={() => this.setState({ action: 'UNINSTALL' })} fluid>Uninstall</Button>
+                <Dropdown options={options} style={{ width: 29 }} floating button className="icon" />
+            </Button.Group>
+        );
+    }
+
     render() {
         return (
             <Card>
@@ -52,11 +95,7 @@ class Application extends React.Component {
                     <Card.Meta>{this.props.category}</Card.Meta>
                     { this.renderDescription() }
                 </Card.Content>
-                <Card.Content extra>
-                    <div className="ui two buttons">
-                        <Button color="red" onClick={() => this.setState({ action: 'UNINSTALL' })}><Icon name="delete" /> Uninstall</Button>
-                    </div>
-                </Card.Content>
+                <Card.Content extra>{ this.renderButtons() }</Card.Content>
                 <Modal size="large" open={this.state.action === 'UNINSTALL'} onClose={() => this.setState({ action: '' })} key="uninstall" basic>
                     <Header icon="delete" content="Uninstall application" />
                     <Modal.Content><p>Are you sure you want to uninstall this application?</p></Modal.Content>
