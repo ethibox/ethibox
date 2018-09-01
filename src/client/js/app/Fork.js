d5eb8bf71a2765ce13e5ac481f1ff6ed15fd8494
ston3o <contact@ston3o.me>
2018-09-01 16:44:02 +0200

    Update repository url

--- src/client/js/app/Fork.js
+++ src/client/js/app/Fork.js
@@ -3,7 +3,7 @@ import forkMe from '../../images/fork-me.png';
 
 export default () => {
     return (
-        <a href="https://github.com/ston3o/ethibox" target="_blank" rel="noopener noreferrer" className="fork">
+        <a href="https://github.com/ethibox/ethibox" target="_blank" rel="noopener noreferrer" className="fork">
             <img style={{ position: 'absolute', top: 0, right: 0, border: 0, zIndex: 100 }} src={forkMe} alt="fork-me" />
         </a>
     );
