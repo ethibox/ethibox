006dc32e22b82d966746c23b3a3c08ff74cfeaff
johackim <contact@johackim.com>
2021-03-22 19:30:24 +0100

    Add SEPA payment method

--- test/store.spec.js
+++ test/store.spec.js
@@ -104,4 +104,34 @@ describe('Store Page', () => {
             cy.get('.grid > div:first-child').contains('Installing...');
         });
     });
+
+    it('Should not show payment card form if SEPA is enabled', () => {
+        cy.request('POST', 'http://localhost:3000/test/reset');
+        cy.request('POST', 'http://localhost:3000/test/import');
+
+        cy.request('POST', 'http://localhost:3000/test/settings', { settings: [
+            { name: 'stripeEnabled', value: 'true' },
+            { name: 'stripePublishableKey', value: Cypress.env('STRIPE_PUBLISHABLE_KEY') },
+            { name: 'stripeSecretKey', value: Cypress.env('STRIPE_SECRET_KEY') },
+        ] });
+
+        cy.request('POST', 'http://localhost:3000/test/users', { users: [{ id: 1, email: 'user@ethibox.fr', password: 'myp@ssw0rd' }] });
+
+        cy.visit('/settings');
+        cy.get('#first_name').clear().type('Marty');
+        cy.get('#last_name').clear().type('Mcfly');
+
+        cy.get('#enable-iban').click();
+        cy.getWithinIframe('input[name="iban"]').type('FR1420041010050500013M02606');
+
+        cy.get('main > div:nth-child(2) button:last').click();
+        cy.contains('.notification', 'Account informations saved successfully');
+
+        cy.visit('/');
+
+        cy.get('.grid > div:last-child button').click();
+        cy.get('#start_free_trial').click();
+
+        cy.url().should('contain', '/apps');
+    });
 });
