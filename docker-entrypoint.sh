f4a324eb46e066159bf05197562e9ecc50a49f4b
johackim <contact@johackim.com>
2021-08-16 23:24:18 +0200

    Update prisma

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,7 @@ find . -maxdepth 1 -name schema.prisma -exec mv -f {} /app/prisma/ \;
 if [ -n "$MYSQL_HOST" ] && [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_PASSWORD" ] && [ -n "$MYSQL_DATABASE" ]; then
     sed -i -e "s/sqlite/mysql/" /app/prisma/schema.prisma
     sed -i -e "s#file:./data.db#mysql://$MYSQL_USER:$MYSQL_PASSWORD@$MYSQL_HOST:3306/$MYSQL_DATABASE#g" /app/prisma/schema.prisma
+    sed -i -e 's/envs          String?/envs          String?       @db.Text/g' /app/prisma/schema.prisma
 fi
 
 if [ -n "$SECRET" ]; then
