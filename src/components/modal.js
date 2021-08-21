9dcc5144c6085bf91609ee0bfdb79f66f56ae1c2
johackim <contact@johackim.com>
2021-08-22 00:06:17 +0200

    Add modal loading

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useIntl } from 'gatsby-plugin-intl';
+import { withPrefix } from 'gatsby';
 
-export default (props) => {
-    const { content, confirmButton, closeButton, confirmClass, onClose, onConfirm } = props;
+export default ({ content, confirmButton, closeButton, confirmClass, onClose, onConfirm, isLoading }) => {
     const intl = useIntl();
 
     return (
@@ -17,14 +17,23 @@ export default (props) => {
                     { closeButton && (
                         <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0">
                             <button onClick={onClose} type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                                {closeButton || intl.formatMessage({ id: 'Cancel' })}
+                                {closeButton}
                             </button>
                         </span>
                     ) }
                     { confirmButton && (
                         <span className="flex w-full rounded-md shadow-sm">
                             <button onClick={onConfirm} type="button" className={`inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-gray-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-gray-500 focus:outline-none focus:border-gray-700 focus:shadow-outline-gray transition ease-in-out duration-150 sm:text-sm sm:leading-5 ${confirmClass}`}>
-                                {confirmButton || 'Confirm'}
+                                { isLoading ? (
+                                    <>
+                                        <span className="w-4 h-4 mr-2">
+                                            <img src={`${withPrefix('/spinner-black.svg')}`} alt="spinner" className="spinner" />
+                                        </span>
+                                        {intl.formatMessage({ id: 'Loading...' })}
+                                    </>
+                                ) : (
+                                    <span>{confirmButton}</span>
+                                ) }
                             </button>
                         </span>
                     ) }
